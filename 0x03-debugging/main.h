76e083936ae3e60197f095c16ad16bb70776262e
Maryam <maryamshile@gmail.com>
2023-03-23 16:20:54 -0700

    0-main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,27 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * positive_or_negative - returns if integer is positive or negative
- * Description: returns if integer is postive of negative
- */
-
 #include <stdio.h>
 
-void positive_or_negative(int i)
-{
-	if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else if (i == 0)
-	{
-		printf("%d is zero\n", i)
-;
-	}
-	else
-	{
-		printf("%d is negative\n", i);
-	}
-}
+void positive_or_negative(int i);
+
 #endif /* MAIN_H */
