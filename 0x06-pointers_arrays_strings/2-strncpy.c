eedd89b071fc9a4af41ca9f9c0dbf9d8b6d74921
Maryam <maryamshile@gmail.com>
2023-03-30 04:28:18 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncpy - copies strings to strings
+ * @dest: pointer variable
+ * @src: pointer variable
+ * @n: integer variable
+ * Return: always 0
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+		dest[i + 1] = '\0';
+	return (dest);
+}
