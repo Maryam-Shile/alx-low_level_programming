dbb858a046af3f9dededca25129ded9ddaee5f79
Maryam <maryamshile@gmail.com>
2023-04-10 07:42:45 -0700

    this code prints the file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - main function
+ *@argc: argument counts
+ *@argv: argument arrays
+ *Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+		
