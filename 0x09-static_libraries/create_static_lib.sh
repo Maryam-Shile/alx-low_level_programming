d5acedca3e7a40f2dee53c50ca9acb16a79a1bf7
Maryam <maryamshile@gmail.com>
2023-04-10 00:52:56 -0700

    create static kib with bash script

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc -c 0-isupper.c 
+gcc -c 0-isupper.c
 gcc -c 0-strcat.c
 gcc -c 1-isdigit.c
 gcc -c 1-strncat.c
@@ -8,7 +8,7 @@ gcc -c 3-islower.c
 gcc -c 3-strcmp.c
 gcc -c 4-isalpha.c
 gcc -c 5-strstr.c
-gcc -c 9-strcpy.c  
+gcc -c 9-strcpy.c
 gcc -c _putchar.c
 gcc -c 0-memset.c
 gcc -c 100-atoi.c
