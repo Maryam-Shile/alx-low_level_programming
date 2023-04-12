8efb4e42aa40225622a909e49d96a6457a715659
Maryam <maryamshile@gmail.com>
2023-04-12 05:42:40 -0700

    modified while loop to be less than length

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -39,17 +39,17 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	while (s1[i] != '\0')
+	while (s1[i] < len1)
 	{
 		arr[i] = s1[i];
 		i++;
 	}
-	while (s2[j] != '\0')
+	while (s2[j] < len2)
 	{
 		arr[i] = s2[j];
 		i++;
 		j++;
 	}
 	arr[i] = '\0';
-	return (arr);
+	return (arri);
 }
