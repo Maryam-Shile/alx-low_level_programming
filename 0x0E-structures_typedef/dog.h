63bfa18d2e9ea5d238659587cbe558c3c5e4d250
Maryam <maryamshile@gmail.com>
2023-04-18 16:58:39 -0700

    struct dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,6 +18,23 @@ struct dog
 	char *owner;
 };
 
+/**
+ * struct dog_t - new struct type
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: defined a new name for
+ * type struct dog
+ */
+
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
