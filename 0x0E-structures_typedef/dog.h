997b7bfad1abe9d4651e77fc6bf6df0eac7609a1
Maryam <maryamshile@gmail.com>
2023-04-18 13:58:04 -0700

    added comments

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog information
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: This struct has 3 members with
+ * information about dogs
+ */
+
 struct dog
 {
 	char *name;
