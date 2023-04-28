9957b7d46cec040f63d0d64fc522a0a5687bf0cc
Maryam Adegboyega <maryamshile@gmail.com>
2023-04-27 20:25:22 -0700

    print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints linked lists
+ * @h: pointer to list h
+ * Return: returns count
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0](nil)\n");
+		}
+		else
+		{
+			printf("[%d]%s\n", h->len, h->str);
+		}
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
+
