0833c80bdd161d33857eaeda3267a20c669342a3
Maryam Adegboyega <maryamshile@gmail.com>
2023-05-05 21:23:34 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,17 +10,17 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-        unsigned long int i;
-        unsigned int flip = 0;
-        unsigned long int mask = 1;
+	unsigned long int i;
+	unsigned int flip = 0;
+	unsigned long int mask = 1;
 
-        for (i = 0; i < sizeof(unsigned long int) * 8; i++)
-        {
-                if ((mask & n) != (mask & m))
-                {
-                        flip++;
-                }
-                mask <<= 1;
-        }
-        return (flip);
+	for (i = 0; i < sizeof(unsigned long int) * 8; i++)
+	{
+		if ((mask & n) != (mask & m))
+		{
+			flip++;
+		}
+		mask <<= 1;
+	}
+	return (flip);
 }
