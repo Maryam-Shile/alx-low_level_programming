8b6a98df318066570fa6e59c695613f303ad633c
Maryam Adegboyega <maryamshile@gmail.com>
2023-05-09 14:32:50 -0700

    2-append_text_to_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,12 +14,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	bw = write(fd, text_content, strlen(text_content));
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 	if (bw == -1  || (size_t)bw != strlen(text_content))
 	{
 		close(fd);
