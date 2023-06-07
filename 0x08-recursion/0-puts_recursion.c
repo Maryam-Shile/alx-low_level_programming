b70877fd65becc39c44dea338c26529eecaa4364
Maryam Adegboyega <maryamshile@gmail.com>
2023-06-07 11:18:46 -0700

    betty errors

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * _puts_recursion - prints a string using recursion
+ * @s: string to be printed
+ */
+
 void _puts_recursion(char *s)
 {
 	int i;
@@ -10,5 +15,4 @@ void _puts_recursion(char *s)
 		putchar(s[i]);
 	}
 	putchar('\n');
-	return;
 }
