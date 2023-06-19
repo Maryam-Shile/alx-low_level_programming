b5a4ef8c1ad2f5f7594d2224adcb8ce27ca4469b
Maryam Adegboyega <maryamshile@gmail.com>
2023-06-19 13:27:15 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *  main - main function
+ *  @void: no arguments
+ *  Return: returns 0 when successful
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
