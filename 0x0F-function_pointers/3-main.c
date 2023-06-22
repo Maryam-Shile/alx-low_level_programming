88095e72dc6cbb51981974e8d985ac60db483984
Maryam Adegboyega <maryamshile@gmail.com>
2023-06-22 02:48:50 -0700

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,15 +12,14 @@
 
 int main(int argc, char *argv[])
 {
-	int ans;
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	ans = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
-	if (ans == 0)
+	if (strcmp(argv[2], "%") != 0 && strcmp(argv[2], "+") != 0
+			&& strcmp(argv[2], "-") != 0 && strcmp(argv[2], "*") != 0
+			&& strcmp(argv[2], "/") != 0)
 	{
 		printf("Error\n");
 		exit(99);
@@ -31,6 +30,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", ans);
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
